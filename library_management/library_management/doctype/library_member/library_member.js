0f1a274c979f219e025e24476591f9d336670634
Jaideep <jaideeprda@gmail.com>
2025-04-21 18:34:46 +0530

    sixth commit

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -19,6 +19,58 @@ frappe.ui.form.on("Library Member", {
           library_member: frm.doc.name,
         });
       });
-    },
+    // },
+    // validate:function(frm){
+    //     frm.set_value('full_name',frm.doc.first_name +" " + frm.doc.last_name)
+    // },
+    // test_fun(frm){
+    //     frappe.msgprint(__('this is mssage'))
+        
+    }
+    //   enable:function(frm){
+    //       let row = frm.add_child('date_and_values_section',{
+    //         date:"01:02:2021",
+    //         value_1:10,
+    //         value_2:20,
+    //       })
+    // }
   });
    
+
+
+
+
+//   frappe.ui.form.on('Library Member', {
+//     refresh: function(frm) {
+//         if (frm.is_new()) {
+//             let d = new frappe.ui.Dialog({
+//                 title: 'Enter the Parent Details',
+//                 fields: [
+//                     {
+//                         label: 'First Name',
+//                         fieldname: 'first_name',
+//                         fieldtype: 'Data'
+//                     },
+//                     {
+//                         label: 'Last Name',
+//                         fieldname: 'last_name',
+//                         fieldtype: 'Data'
+//                     },
+//                     // {
+//                     //     label: 'Email Address',
+//                     //     fieldname: 'email_address',
+//                     //     fieldtype: 'Data'
+//                     // }
+//                 ],
+//                 primary_action_label: 'Submit',
+//                 primary_action(values) {
+//                     frm.set_value('first_name', values.first_name);
+//                     frm.set_value('last_name', values.last_name);
+//                     // frm.set_value('Email Address', values.email_address);
+//                     d.hide();
+//                 }
+//             });
+//             d.show();
+//         }
+//     }
+// });
--- library_management/library_management/doctype/library_transaction/library_transaction.js
+++ library_management/library_management/doctype/library_transaction/library_transaction.js
@@ -6,3 +6,22 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Library Transaction", {
+	// refresh(frm) {
+    // before_save:function(frm){
+    //     frappe.throw("hello before save in data")
+    // }
+
+    // after_save:function(frm){
+    //     frappe.throw("hello from after_save in data")
+    // }
+    // enble_save:function(frm){
+    //     frappe.throw("hello from enble_save in data")
+    // }
+	// },
+    refresh:function(frm){
+        frm.is_new("kkkk")
+        frm.set_intro("hello from enble_save in data")
+    }
+});
